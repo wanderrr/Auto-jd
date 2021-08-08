b44d76a2e3bf8efa22fe7c3e4ea81c3c3db6e28a
wanderrr <wanderrr20@gmail.com>
2021-08-09 01:10:18 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -3,7 +3,7 @@
  */
 //此处填写京东账号cookie。
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJhEAv8ADDOKLOcjc2toTaIgZ4QeYg6yaMEbJvno8ShqxCZKx28idfRg2jZjLab_pM99jY-Vtg;pt_pin=2rIDVJHZITrbXrUjzBWQTQ;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断环境变量里面是否有京东ck
